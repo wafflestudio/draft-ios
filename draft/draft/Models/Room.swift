3c3866460ca607bc10ee44116732e2393a6e342b
JSKeum <JSKeum2512@gmail.com>
2020-05-04 13:52:34 +0900

    Create basic structure of two models - Room & RoomGroup

---
+++ draft/draft/Models/Room.swift
@@ -0,0 +1,9 @@
+//
+//  Room.swift
+//  draft
+//
+//  Created by JSKeum on 2020/05/03.
+//  Copyright © 2020 JSKeum. All rights reserved.
+//
+
+import Foundation
---
+++ draft/draft/Models/RoomGroup.swift
@@ -0,0 +1,9 @@
+//
+//  RoomGroup.swift
+//  draft
+//
+//  Created by JSKeum on 2020/05/03.
+//  Copyright © 2020 JSKeum. All rights reserved.
+//
+
+import Foundation
