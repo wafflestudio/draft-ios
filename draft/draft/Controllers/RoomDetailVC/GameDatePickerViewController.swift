c106525282d5184f7c0f495f4b6e9e9750e2d554
JSKeum <JSKeum2512@gmail.com>
2020-07-18 02:22:40 +0900

    Add DatePicker Controller

---
+++ draft/draft/Controllers/RoomDetailVC/GameDatePickerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameDatePickerViewController.swift
+//  draft
+//
+//  Created by JSKeum on 2020/07/18.
+//  Copyright © 2020 JSKeum. All rights reserved.
+//
+
+import UIKit
+
+class GameDatePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        
+    }
+    
+    @IBAction func cancel(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func choose(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+}
