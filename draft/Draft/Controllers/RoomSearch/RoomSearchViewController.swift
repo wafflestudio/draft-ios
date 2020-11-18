08a524392b1ab3da65339ba11db53b3181a2a668
MoBeau <shhan098@naver.com>
2020-11-18 21:41:45 +0900

    RoomSearchView 뼈대 완성

---
+++ draft/Draft/Controllers/RoomSearch/RoomSearchViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RoomSearchViewController.swift
+//  Draft
+//
+//  Created by 한상현 on 2020/11/18.
+//  Copyright © 2020 JSKeum. All rights reserved.
+//
+
+import UIKit
+
+class RoomSearchViewController: UIViewController {
+
+    @IBOutlet weak var filterBGView: UIView!
+    @IBOutlet weak var searchResultTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        prepareView()
+        // Do any additional setup after loading the view.
+    }
+    
+    func prepareView(){
+        filterBGView.layer.cornerRadius = 10
+        searchResultTableView.layer.cornerRadius = 10
+    }
+}
