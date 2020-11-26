935d07f7a2fb2aa7d94926deaa21fed84822adee
Ethan-MoBeau <Ethan@dunamu.com>
2020-11-26 20:53:00 +0900

    [#41] RoomSearchViewModel 추가
    - 파일 추가

---
+++ draft/Draft/Controllers/RoomSearch/RoomSearchViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  RoomSearchViewModel.swift
+//  Draft
+//
+//  Created by Han Sang Hyeon - Ethan on 2020/11/26.
+//  Copyright © 2020 JSKeum. All rights reserved.
+//
+
+class RoomSearchViewModel {
+    
+}
